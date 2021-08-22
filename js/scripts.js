382cda71fbb56ac667beb9f1f25efa2c7e4eaf73
chinchen-lab <wachin@gapp.nthu.edu.tw>
2021-08-23 02:20:01 +0800

    js

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function () {
+    window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if (currentTop < scrollPos) {
+        if ( currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
@@ -28,10 +28,4 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 })
 
-
-document.ready(function () {
-    setTimeout(function () {
-        $(".loading-wrapper").fadeOut(500);
-    }, 2000);
-});
 
